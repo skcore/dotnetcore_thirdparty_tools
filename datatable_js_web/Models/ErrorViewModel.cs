ed82e4e13b21d69c412730bf95cd30657ac5d28f
skcore <skcore1013@gmail.com>
2024-01-18 21:50:57 +0530

    datatable-js

---
+++ datatable_js_web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace datatable_js_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
